db8dec8b3d6b1a7bb72a9fc02ba9cce298af7c51
Joao Victor <jfarchi@sga.pucminas.br>
2024-06-19 23:25:00 -0300

    implementacao visual do xadrez

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,1 +1,1 @@
-package application;import boardgame.Board;import boardgame.Position;public class Program {    public static void main(String[] args) {        Board board = new Board(8, 8);    }}
+package application;import chess.ChessMatch;public class Program {    public static void main(String[] args) {        ChessMatch chessMatch = new ChessMatch();        UI.printBoard(chessMatch.getPieces());    }}
