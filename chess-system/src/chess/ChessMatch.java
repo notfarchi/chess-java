e04ed165632482650630b3dd97f8268e5eb383f4
Joao Victor <jfarchi@sga.pucminas.br>
2024-06-27 13:54:49 -0300

    aplicando throw com a negacao do metodo isThereAnyPossibleMove()

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -42,6 +42,8 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("Nao existe peca na posicao de origem!");
+        } if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("Nao ha movimentos possiveis para a peca escolhida!");
         }
     }
 
